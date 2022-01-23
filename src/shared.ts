64722a3fb61d0a4151f038b3e6a4697852ffee1e
Davey van den Bogaard <daveyvdbogaard@live.nl>
2022-01-23 17:33:03 +0100

    feat: begin of generating points from qeury params

--- src/shared.ts
+++ src/shared.ts
@@ -2,10 +2,25 @@ import { GradientConfig, GradientPoint } from "./types";
 
 export const processParams = (queryParams: any): GradientConfig => {
   const formattedPoints: GradientPoint[] = [];
+
   return {
     width: Number(queryParams.width),
     height: Number(queryParams.height),
     noise: queryParams.noise === "true",
     points: formattedPoints,
   };
+};
+
+const generatePoints = (points: any): GradientPoint[] => {
+  // FROM
+  // x: [ '0', '500' ],
+  // y: [ '0', '300' ],
+  // color: [ '#FFFFFF', '#FAB555' ]
+  // TO
+  // [
+  // { x: 0, y: 0, color: "#FFFFFF" },
+  // { x: 500, y: 300, color: "#FAB555" },
+  //   ]
+
+  return [];
 };
